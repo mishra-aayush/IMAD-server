8e095b7a12b3c421ee638436548b1830f871eddf
Aayush Mishra <48150464+mishra-aayush@users.noreply.github.com>
2019-11-13 10:59:51 +0530

    Add files via upload

--- lib/config_paytm.php
+++ lib/config_paytm.php
@@ -1,5 +1,4 @@
 <?php
 //Change the value of PAYTM_MERCHANT_KEY constant with details received from Paytm.
-define('PAYTM_MERCHANT_KEY', 'BRHIdWLLTNy84%A&
-'); 
+define('PAYTM_MERCHANT_KEY', 'BRHIdWLLTNy84%A&'); 
 ?>
